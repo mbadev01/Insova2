93a0ba6e0a8de38a66d56d5f2dc61e01ac77d8cf
ilal Azhar <artivid.studio@gmail.com>
2023-09-16 01:33:34 +0500

    Make services section

--- src/App.jsx
+++ src/App.jsx
@@ -257,7 +257,91 @@ const handleInputChange = (event) => {
                 </div>
                 <div>Years Experience</div>
               </div>
-              
+            </div>
+          </div>
+        </section>
+
+        {/* Services */}
+        <section className="services">
+          <div
+            className="bg-services bg-cover bg-no-repeat max-w-[1466px] mx-4 xl:mx-auto rounded-[20px] xl:pt-[70px]
+          px-6 xl:px-0 relative h-[368px] flex items-center xl:items-start -z-10
+          "
+          >
+            <div className="container mx-auto">
+              <div className="services__top flex items-center flex-col xl:flex-row xl:mb-[60px]">
+                <h2 className="h2 text-white flex-1 mb-4 xl:mb-0 text-center xl:text-left">
+                  Our best Services for your solution
+                </h2>
+                <p className="text-white flex-1 text-center xl:text-left max-w-2xl xl:max-w-none">
+                  Lorem Ipsum has been the standard dummy text ever since the
+                  1500s, when an unknown printer took a galley of type and
+                  scrambled it to make a type specimen book. It has survived not
+                  only five centuries, but also the leap into electronic
+                  typesetting, remaining essentially unchanged.
+                </p>
+              </div>
+            </div>
+          </div>
+          {/* grid container */}
+          <div className="container mx-auto mt-8  xl:-mt-[144px]">
+            <div className="grid xl:grid-cols-4 gap-5 px-8 xl:px-0">
+              <div
+                className="services__item bg-white p-[30px] rounded-[10px] shadow-custom2 min-h-[288px] flex flex-col
+            items-center text-center
+            "
+              >
+                <div className="mb-[15px]">
+                  <img src="/assets/icons/services/icon-1.svg" />
+                </div>
+                <h3 className="h3 mb-[10px]">General Practitioners</h3>
+                <p className="font-light leading-normal max-w-[300px] ">
+                  Aliquam etiam lectus adipising est auctor mi quisque non
+                  viverra
+                </p>
+              </div>
+              <div
+                className="services__item bg-white p-[30px] rounded-[10px] shadow-custom2 min-h-[288px] flex flex-col
+            items-center text-center
+            "
+              >
+                <div className="mb-[15px]">
+                  <img src="/assets/icons/services/icon-2.svg" />
+                </div>
+                <h3 className="h3 mb-[10px]">Pregnancy Support</h3>
+                <p className="font-light leading-normal max-w-[300px] ">
+                  Aliquam etiam lectus adipising est auctor mi quisque non
+                  viverra
+                </p>
+              </div>
+              <div
+                className="services__item bg-white p-[30px] rounded-[10px] shadow-custom2 min-h-[288px] flex flex-col
+            items-center text-center
+            "
+              >
+                <div className="mb-[15px]">
+                  <img src="/assets/icons/services/icon-3.svg" />
+                </div>
+                <h3 className="h3 mb-[10px]">Nutritional Support</h3>
+                <p className="font-light leading-normal max-w-[300px] ">
+                  Aliquam etiam lectus adipising est auctor mi quisque non
+                  viverra
+                </p>
+              </div>
+              <div
+                className="services__item bg-white p-[30px] rounded-[10px] shadow-custom2 min-h-[288px] flex flex-col
+            items-center text-center
+            "
+              >
+                <div className="mb-[15px]">
+                  <img src="/assets/icons/services/icon-4.svg" />
+                </div>
+                <h3 className="h3 mb-[10px]">Pharmaceutical Care</h3>
+                <p className="font-light leading-normal max-w-[300px] ">
+                  Aliquam etiam lectus adipising est auctor mi quisque non
+                  viverra
+                </p>
+              </div>
             </div>
           </div>
         </section>
